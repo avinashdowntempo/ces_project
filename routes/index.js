ff7bacaf6f825b0fa29703a058ab02d1c71b403b
avinashdowntempo <avinashj4394@gmail.com>
2017-05-31 03:41:06 +0530

    added fb post

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var User=require('../lib/User');
 var Career=require('../lib/Career');
+var FB = require('facebook-node');
+FB.setApiVersion("v2.2");
+FB.setAccessToken('EAACEdEose0cBAIYzGQI9VtZCFYcJfFOjuWmd24DtAEHZCtCO9ZC7ES5t6SgkBRxDLRljMJO4ZCOaZAfkCMGKp1gvWgzZBl383aR9X7AvLUhLRBiGrtNAh0Yx9eFThUkIYI4jz2KBzb1tDXeLNqwBHOhyMkzu7X7MmxHShneITPnf2dcT7M0xHVFd0ZCvhVigXsZD');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -40,11 +43,14 @@ router.post('/career', function(req,res){
   var technology=req.body.technology;
   var minexp=req.body.minexp;
   var maxexp= req.body.maxexp;
+  console.log(req.body.technology);
   var description= req.body.description;
   var email= req.body.email;
   var city= req.body.city;
   var location= req.body.location;
   var newcareer=new Career();
+  var post='job openings \n Designation: '+title.concat('\n experience: ',minexp,' to  ',maxexp,' years \n description: ',description,'\n email: ',email,'\n city: ',city,'\n location:',location);
+  console.log(post);
   newcareer.title=title;
    newcareer.technology=technology;
    newcareer.minexp=minexp;
@@ -58,7 +64,16 @@ router.post('/career', function(req,res){
       console.log(err);
       return res.status(500).send();
     }
+        var body = 'My first post using facebook-node';
+   FB.api('me/feed', 'post', { message: post}, function (res) {
+  if(!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+  console.log('Post Id: ' + res.id);
+});
     return res.status(200).send();
+
   })
 })
 router.post('/register', function(req,res){
